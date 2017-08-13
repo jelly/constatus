0888f7f35524077062686a821b8cd0dcf743adb0
Folkert van Heusden <mail@vanheusden.com>
2017-08-13 10:25:02 +0200

    segfault fix in demoocv

--- ext-filters/demoocv.cpp
+++ ext-filters/demoocv.cpp
@@ -17,7 +17,7 @@ void init_filter(const char *const parameter)
 
 void apply_filter(const uint64_t ts, const int w, const int h, const uint8_t *const prev_frame, const uint8_t *const current_frame, uint8_t *const result)
 {
-	cv::Mat imageWithData = cv::Mat(w * h * 3, 1, CV_8UC3, (void *)current_frame);
+	cv::Mat imageWithData = cv::Mat(w * h, 1, CV_8UC3, (void *)current_frame).clone();
 	cv::Mat reshapedImage = imageWithData.reshape(0, h);
 
 	cv::Mat invSrc = cv::Scalar::all(255) - reshapedImage;
