742ac08e2e9b5618377bcc8b776148ad380bad6c
Folkert van Heusden <folkert@vanheusden.com>
2017-08-28 20:38:45 +0200

    renamed filter_ext to filter_plugin

--- filter_plugin.cpp
+++ filter_plugin.cpp
@@ -3,9 +3,9 @@
 #include <string.h>
 
 #include "error.h"
-#include "filter_ext.h"
+#include "filter_plugin.h"
 
-filter_ext::filter_ext(const std::string & filter_filename, const std::string & parameter)
+filter_plugin::filter_plugin(const std::string & filter_filename, const std::string & parameter)
 {
 	library = dlopen(filter_filename.c_str(), RTLD_NOW);
 	if (!library)
@@ -26,14 +26,14 @@ filter_ext::filter_ext(const std::string & filter_filename, const std::string & 
 	arg = init_filter(parameter.c_str());
 }
 
-filter_ext::~filter_ext()
+filter_plugin::~filter_plugin()
 {
 	uninit_filter(arg);
 
 	dlclose(library);
 }
 
-void filter_ext::apply_io(const uint64_t ts, const int w, const int h, const uint8_t *const prev, const uint8_t *const in, uint8_t *const out)
+void filter_plugin::apply_io(const uint64_t ts, const int w, const int h, const uint8_t *const prev, const uint8_t *const in, uint8_t *const out)
 {
 	apply_filter(arg, ts, w, h, prev, in, out);
 }
--- filter_plugin.h
+++ filter_plugin.h
@@ -7,7 +7,7 @@ typedef void * (*init_filter_t)(const char *const par);
 typedef void (*apply_filter_t)(void *arg, const uint64_t ts, const int w, const int h, const uint8_t *const prev_frame, const uint8_t *const current_frame, uint8_t *const result);
 typedef void (*uninit_filter_t)(void *arg);
 
-class filter_ext : public filter
+class filter_plugin : public filter
 {
 private:
 	void *library;
@@ -17,8 +17,8 @@ private:
 	void *arg;
 
 public:
-	filter_ext(const std::string & filter_filename, const std::string & parameter);
-	~filter_ext();
+	filter_plugin(const std::string & filter_filename, const std::string & parameter);
+	~filter_plugin();
 
 	bool uses_in_out() const { return true; }
 	void apply_io(const uint64_t ts, const int w, const int h, const uint8_t *const prev, const uint8_t *const in, uint8_t *const out);
