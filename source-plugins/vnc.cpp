2bcea5f9fb90e5cacaa3de84853761737911f9a7
Folkert van Heusden <folkert@vanheusden.com>
2017-09-28 19:19:43 +0200

    Working on vnc client plugin

--- source-plugins/vnc.cpp
+++ source-plugins/vnc.cpp
@@ -37,23 +37,23 @@ static void reconnect(const std::string & host, const int port, std::atomic_bool
 
 		log(LL_DEBUG(host_details + ": protocol version: " + ProtocolVersion);
 
-		// send VNC-client protocol version
-		char my_prot_version[] = "RFB 003.003\n";
-		if (WRITE(*fd, my_prot_version, 12) != 12) {
-			log(LL_WARNING, host_details + ": protocol version handshake short write (" + get_errno_string() + ")");
-			continue;
-		}
+				// send VNC-client protocol version
+				char my_prot_version[] = "RFB 003.003\n";
+				if (WRITE(*fd, my_prot_version, 12) != 12) {
+				log(LL_WARNING, host_details + ": protocol version handshake short write (" + get_errno_string() + ")");
+				continue;
+				}
 
-		char auth[4];
-		if (READ(*fd, auth, 4) != 4) {
-			log(LL_WARNING, host_details + ": auth method short read (" + get_errno_string() + ")");
-			continue;
-		}
+				char auth[4];
+				if (READ(*fd, auth, 4) != 4) {
+				log(LL_WARNING, host_details + ": auth method short read (" + get_errno_string() + ")");
+				continue;
+				}
 
-		if (auth[3] == 0) { // CONNECTION FAILED
-			std::string reason;
+				if (auth[3] == 0) { // CONNECTION FAILED
+				std::string reason;
 
-			for(;;) {
+				for(;;) {
 				unsigned char auth_err_len[4];
 				if (READ(*fd, reinterpret_cast<char *>(auth_err_len), 4) != 4)
 					break;
@@ -72,128 +72,128 @@ static void reconnect(const std::string & host, const int port, std::atomic_bool
 				delete [] msg;
 
 				break;
-			}
+				}
 
-			log(LL_WARNING, host_details + ": connection failed (" + reason + ")");
+				log(LL_WARNING, host_details + ": connection failed (" + reason + ")");
 
-			continue;
-		}
-		else if (auth[3] == 1) { // NO AUTHENTICATION
-			// do nothing
-			log(LL_INFO, host_details + ": log-on without password");
-		}
+				continue;
+				}
+				else if (auth[3] == 1) { // NO AUTHENTICATION
+					// do nothing
+					log(LL_INFO, host_details + ": log-on without password");
+				}
 #if 0
-		else if (auth[3] == 2) // AUTHENTICATION
-		{
-			unsigned char challenge[16 + 1];
-			if (READ(*fd, reinterpret_cast<char *>(challenge), 16) != 16)
-			{
-				error(host_details + ": challenge short read (" + get_errno_string() + ")");
-				break;
-			}
+				else if (auth[3] == 2) // AUTHENTICATION
+				{
+					unsigned char challenge[16 + 1];
+					if (READ(*fd, reinterpret_cast<char *>(challenge), 16) != 16)
+					{
+						error(host_details + ": challenge short read (" + get_errno_string() + ")");
+						break;
+					}
 
-			// retrieve first 8 characters of password and pad with 0x00
-			const_DES_cblock password_padded;
-			memset(password_padded, 0x00, sizeof password_padded);
-			memcpy(password_padded, vnc_password.c_str(), std::min(8, int(vnc_password.size())));
-			// then vnc swaps the bits in the password
-			for(int index=0; index<8; index++)
-			{ 
-				int cur = password_padded[index];
-				password_padded[index] = static_cast<unsigned char>(((cur & 0x01)<<7) + ((cur & 0x02)<<5) + ((cur & 0x04)<<3) + ((cur & 0x08)<<1) + ((cur & 0x10)>>1) + ((cur & 0x20)>>3) + ((cur & 0x40)>>5) + ((cur & 0x80)>>7));
-			}
-			// setup key schedule
-			DES_key_schedule key_schedule;
-			DES_set_key_unchecked(&password_padded, &key_schedule);
+					// retrieve first 8 characters of password and pad with 0x00
+					const_DES_cblock password_padded;
+					memset(password_padded, 0x00, sizeof password_padded);
+					memcpy(password_padded, vnc_password.c_str(), std::min(8, int(vnc_password.size())));
+					// then vnc swaps the bits in the password
+					for(int index=0; index<8; index++)
+					{ 
+						int cur = password_padded[index];
+						password_padded[index] = static_cast<unsigned char>(((cur & 0x01)<<7) + ((cur & 0x02)<<5) + ((cur & 0x04)<<3) + ((cur & 0x08)<<1) + ((cur & 0x10)>>1) + ((cur & 0x20)>>3) + ((cur & 0x40)>>5) + ((cur & 0x80)>>7));
+					}
+					// setup key schedule
+					DES_key_schedule key_schedule;
+					DES_set_key_unchecked(&password_padded, &key_schedule);
 
-			// encrypt
-			unsigned char result[16];
-			const_DES_cblock ch1, ch2;
-			memcpy(ch1, &challenge[0], 8);
-			memcpy(ch2, &challenge[8], 8);
-			DES_cblock r1, r2;
-			DES_ecb_encrypt(&ch1, &r1, &key_schedule, DES_ENCRYPT);
-			DES_ecb_encrypt(&ch2, &r2, &key_schedule, DES_ENCRYPT);
-			memcpy(&result[0], r1, 8);
-			memcpy(&result[8], r2, 8);
-			// transmit result to server
-			if (WRITE(*fd, reinterpret_cast<char *>(result), 16) != 16)
-			{
-				error(host_details + ": auth response short write (" + get_errno_string() + ")");
-				break;
-			}
+					// encrypt
+					unsigned char result[16];
+					const_DES_cblock ch1, ch2;
+					memcpy(ch1, &challenge[0], 8);
+					memcpy(ch2, &challenge[8], 8);
+					DES_cblock r1, r2;
+					DES_ecb_encrypt(&ch1, &r1, &key_schedule, DES_ENCRYPT);
+					DES_ecb_encrypt(&ch2, &r2, &key_schedule, DES_ENCRYPT);
+					memcpy(&result[0], r1, 8);
+					memcpy(&result[8], r2, 8);
+					// transmit result to server
+					if (WRITE(*fd, reinterpret_cast<char *>(result), 16) != 16)
+					{
+						error(host_details + ": auth response short write (" + get_errno_string() + ")");
+						break;
+					}
 
-			char auth_answer[4];
-			if (READ(*fd, auth_answer, 4) != 4)
-			{
-				error(host_details + ": auth response short read (" + get_errno_string() + ")");
-				break;
-			}
+					char auth_answer[4];
+					if (READ(*fd, auth_answer, 4) != 4)
+					{
+						error(host_details + ": auth response short read (" + get_errno_string() + ")");
+						break;
+					}
 
-			if (auth_answer[3] != 0)
-			{
-				error(host_details + ": authentication failed");
-				break;
-			}
+					if (auth_answer[3] != 0)
+					{
+						error(host_details + ": authentication failed");
+						break;
+					}
 
-			logger -> dolog(host_details + ": authentication succeeded");
-		}
+					logger -> dolog(host_details + ": authentication succeeded");
+				}
 #endif
-		else
-		{
-			log(LL_ERR, host_details + ": connection failed, unknown authentication");
-			continue;
-		}
+				else
+				{
+					log(LL_ERR, host_details + ": connection failed, unknown authentication");
+					continue;
+				}
 
-		char do_shared = 1;
-		if (WRITE(*fd, &do_shared, 1) != 1) {
-			log(LL_ERR, host_details + ": enable shared failed (" + get_errno_string() + ")");
-			continue;
-		}
+				char do_shared = 1;
+				if (WRITE(*fd, &do_shared, 1) != 1) {
+					log(LL_ERR, host_details + ": enable shared failed (" + get_errno_string() + ")");
+					continue;
+				}
 
-		unsigned char parameters[2 + 2 + 16 + 4];
-		if (READ(*fd, reinterpret_cast<char *>(parameters), 24) != 24) {
-			log(LL_ERR, host_details + ": parameters short read (" + get_errno_string() + ")");
-			break;
-		}
+				unsigned char parameters[2 + 2 + 16 + 4];
+				if (READ(*fd, reinterpret_cast<char *>(parameters), 24) != 24) {
+					log(LL_ERR, host_details + ": parameters short read (" + get_errno_string() + ")");
+					break;
+				}
 
-		swap_rgb = parameters[6] != 0;
-		*vnc_width = GET_CARD16(&parameters[0]);
-		*vnc_height = GET_CARD16(&parameters[2]);
+				swap_rgb = parameters[6] != 0;
+				*vnc_width = GET_CARD16(&parameters[0]);
+				*vnc_height = GET_CARD16(&parameters[2]);
 
-		logger -> dolog("%s: w/h: %dx%d", host_details.c_str(), vnc_width, vnc_height);
+				logger -> dolog("%s: w/h: %dx%d", host_details.c_str(), vnc_width, vnc_height);
 
-		char *vnc_name = new char[parameters[23] + 1];
-		if (READ(*fd, vnc_name, parameters[23]) != parameters[23]) {
-			delete [] vnc_name;
-			log(LL_ERR, host_details + ": name short read (" + get_errno_string() + ")");
-			continue;
-		}
+				char *vnc_name = new char[parameters[23] + 1];
+				if (READ(*fd, vnc_name, parameters[23]) != parameters[23]) {
+					delete [] vnc_name;
+					log(LL_ERR, host_details + ": name short read (" + get_errno_string() + ")");
+					continue;
+				}
 
-		vnc_name[parameters[23]] = 0x00;
-		log(LL_DEBUG, host_details + ": session name: " + std::string(vnc_name));
-		delete [] vnc_name;
+				vnc_name[parameters[23]] = 0x00;
+				log(LL_DEBUG, host_details + ": session name: " + std::string(vnc_name));
+				delete [] vnc_name;
 
-		log(LL_DEBUG, "bits per pixel: %d, depth: %d, r/g/b shift: %d/%d/%d", parameters[4], parameters[5], parameters[14], parameters[15], parameters[16]);
+				log(LL_DEBUG, "bits per pixel: %d, depth: %d, r/g/b shift: %d/%d/%d", parameters[4], parameters[5], parameters[14], parameters[15], parameters[16]);
 
-		// printf("%d, %d, %d\n", GET_CARD16(&parameters[4+4]), GET_CARD16(&parameters[6+4]), GET_CARD16(&parameters[8+4]));
-		if (parameters[4] != 32) {
-			log(LL_ERR, host_details + ": only 32 bit/pixel supported");
-			continue;
-		}
+				// printf("%d, %d, %d\n", GET_CARD16(&parameters[4+4]), GET_CARD16(&parameters[6+4]), GET_CARD16(&parameters[8+4]));
+				if (parameters[4] != 32) {
+					log(LL_ERR, host_details + ": only 32 bit/pixel supported");
+					continue;
+				}
 
-		// set encoding
-		// char encoding[] = { 2, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 0 };
-		char encoding[] = { 2, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 0 };
-		if (WRITE(*fd, encoding, sizeof encoding) != sizeof encoding) {
-			log(LL_ERR, host_details + ": short write for encoding message (" + get_errno_string() + ")");
-			continue;
-		}
+				// set encoding
+				// char encoding[] = { 2, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 0 };
+				char encoding[] = { 2, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 0 };
+				if (WRITE(*fd, encoding, sizeof encoding) != sizeof encoding) {
+					log(LL_ERR, host_details + ": short write for encoding message (" + get_errno_string() + ")");
+					continue;
+				}
 
-		log(LL_INFO, host_details + ": session started");
-		incremental = false;
+				log(LL_INFO, host_details + ": session started");
+				incremental = false;
 
-		break;
+				break;
 	}
 }
 
@@ -451,6 +451,8 @@ bool request_update(int cur_fd)
 typedef struct
 {
 	source *s;
+	std::string host;
+	int port;
 	std::atomic_bool stop_flag;
 	pthread_t th;
 } my_data_t;
@@ -463,169 +465,133 @@ void * thread(void *arg)
 
 	while(!md -> stop_flag) {
 		usleep(10000);
-	set_thread_name(name);
 
-	bool msg = true;
-	while(!md -> stop_flag) {
-		if (msg) {
-			data_lock.lock();
-			if (bg)
+		set_thread_name("vnc-client");
+
+		int fd = -1, width = 0, height = 0;
+
+		uint8_t *fb = NULL;
+		size_t bytes = 0;
+
+		bool msg = true;
+		while(!md -> stop_flag) {
+			reconnect(md -> host, md -> port, md -> stop_flag, &fd, &width, &height);
+
+			bytes = width * height * 4;
+			fb = vmalloc(bytes);
+
+			md -> s -> set_size(width, height);
+
+			double cur_ts = get_rel_ts(), end_ts = cur_ts + 1.0 / fps;
+			double start_ts = get_rel_ts();
+			bool upd_req = true;
+			bool got_update = false;
+
+			for(;!md -> stop_flag;)
 			{
-				SDL_Color fg = { 255, 0, 0, 255 };
-				draw_and_put_text(bg, 5, bg -> h / 3, error_font, fg, "Connecting to:", blended, false);
-				draw_and_put_text(bg, 5, (bg -> h / 3) * 2, error_font, fg, host_details.c_str(), blended, false);
-				msg = false;
-			}
-			data_lock.unlock();
-		}
-
-		//printf("reconnect\n");
-		if (fd == -1 && !reconnect())
-		{
-			// this check is so that we don't do a sleep
-			// when we're supposed to exit
-			if (end_thread_flag -> exchange(false))
-				break;
-
-			if (my_usleep(end_thread_flag, reconnect_sleep * 1000000))
-				break;
-			if (reconnect_sleep < 16)
-				reconnect_sleep++;
-			msg = true;
-			continue;
-		}
-		reconnect_sleep = 1;
-
-		double cur_ts = get_rel_ts(), end_ts = cur_ts + 1.0 / fps;
-		double start_ts = get_rel_ts();
-		bool upd_req = true;
-		bool got_update = false;
-		for(;;)
-		{
-			if (upd_req)
-			{
-				upd_req = false;
-
-				if (!request_update(fd))
-					break;
-			}
-
-			struct timeval tv;
-			double ts_diff = std::max(0.001, end_ts - cur_ts);
-			if (ts_diff > EXIT_POLL_MS / 1000.0)
-				ts_diff = EXIT_POLL_MS / 1000.0;
-			tv.tv_sec = long(ts_diff);
-			tv.tv_usec = long((ts_diff - (double)tv.tv_sec) * 1000000.0);
-
-			fd_set rfds;
-			FD_ZERO(&rfds);
-			FD_SET(fd, &rfds);
-
-			if (select(fd + 1, &rfds, NULL, NULL, &tv) == -1)
-			{
-				if (errno == EINTR || errno == EAGAIN)
-					continue;
-
-				close(fd);
-				error(host_details + ": connection problems (" + get_errno_string() + ")");
-
-				break;
-			}
-			cur_ts = get_rel_ts();
-			if (FD_ISSET(fd, &rfds))
-			{
-				if (!draw_vnc(fd, fb))
-					break;
-
-				got_update = true;
-			}
-
-			if (end_thread_flag -> exchange(false))
-			{
-				end_thread_flag -> store(true);
-				break;
-			}
-
-			if (cur_ts >= end_ts && fb != NULL && got_update)
-			{
-				got_update = false;
-
-				upd_req = true;
-
-				if (swap_rgb) 
+				if (upd_req)
 				{
-					for(unsigned int index=0; index<(vnc_width * vnc_height * 4); index += 4)
-					{
-						// unsigned char p1 = ((char *)fb -> pixels)[index + 0];
-						unsigned char p2 = static_cast<char *>(fb -> pixels)[index + 1];
-						unsigned char p3 = static_cast<char *>(fb -> pixels)[index + 2];
-						unsigned char p4 = static_cast<char *>(fb -> pixels)[index + 3];
-						static_cast<char *>(fb -> pixels)[index + 0] = p4;
-						static_cast<char *>(fb -> pixels)[index + 1] = p3;
-						static_cast<char *>(fb -> pixels)[index + 2] = p2;
-						static_cast<char *>(fb -> pixels)[index + 3] = alpha_value;
-					}
-				}
-				else
-				{
-					for(unsigned int index=0; index<(vnc_width * vnc_height * 4); index += 4)
-						static_cast<char *>(fb -> pixels)[index + 3] = alpha_value;
+					upd_req = false;
+
+					if (!request_update(fd))
+						break;
 				}
 
-				tweak_image(static_cast<unsigned char *>(fb -> pixels));
-				set_bitmap(static_cast<unsigned char *>(fb -> pixels), vnc_width, vnc_height);
+				struct timeval tv;
+				double ts_diff = std::max(0.001, end_ts - cur_ts);
 
-				frame_loaded_flag -> store(true);
+				if (ts_diff > 100 / 1000.0)
+					ts_diff = 100 / 1000.0;
+				tv.tv_sec = long(ts_diff);
+				tv.tv_usec = long((ts_diff - (double)tv.tv_sec) * 1000000.0);
 
-				avg_frame_count = update_fps_counter(start_ts, cur_ts);
+				fd_set rfds;
+				FD_ZERO(&rfds);
+				FD_SET(fd, &rfds);
 
-				start_ts = cur_ts;
-				end_ts = cur_ts + 1.0 / fps;
+				if (select(fd + 1, &rfds, NULL, NULL, &tv) == -1)
+				{
+					if (errno == EINTR || errno == EAGAIN)
+						continue;
+
+					log(LL_ERR, host_details + ": connection problems (" + get_errno_string() + ")");
+
+					break;
+				}
+
+				cur_ts = get_rel_ts();
+
+				if (FD_ISSET(fd, &rfds)) {
+					if (!draw_vnc(fd, fb))
+						break;
+
+					got_update = true;
+				}
+
+				if (cur_ts >= end_ts && fb != NULL && got_update) {
+					got_update = false;
+
+					upd_req = true;
+
+					md -> s -> set_frame(E_RGB, fb, bytes);
+
+					avg_frame_count = update_fps_counter(start_ts, cur_ts);
+
+					start_ts = cur_ts;
+					end_ts = cur_ts + 1.0 / fps;
+				}
 			}
+
+			close(fd);
+
+			free(fb);
+			fb = NULL;
 		}
 
-		if (fd != -1)
-		{
+		free(fb);
+
+		if (fd != -1) {
 			close(fd);
 			fd = -1;
 		}
+
+		log(LL_INFO, "source plugin for VNC client thread ending");
+
+		return NULL;
 	}
 
-	if (fd != -1)
+	extern "C" void *init_plugin(const char *const argument)
 	{
-		close(fd);
-		fd = -1;
+		my_data_t *md = new my_data_t;
+		md -> s = s;
+		md -> stop_flag = false;
+
+		md -> address = "localhost";
+		md -> port = 5901;
+
+		if (argument) {
+			md -> address = argument;
+
+			char *sp = strchr(argument, ' ');
+			if (sp) {
+				*sp = 0x00;
+
+				md -> port = atoi(sp + 1);
+			}
+		}
+
+		pthread_create(&md -> th, NULL, thread, md);
+
+		return md;
 	}
 
-	logger -> dolog("Thread for VNC " + name + " ends");
+	extern "C" void uninit_plugin(void *arg)
+	{
+		my_data_t *md = (my_data_t *)arg;
+
+		md -> stop_flag = true;
+
+		pthread_join(md -> th, NULL);
+
+		delete md;
 	}
-
-	free(pic);
-
-	log(LL_INFO, "source plugin thread ending");
-
-	return NULL;
-}
-
-
-extern "C" void *init_plugin(const char *const argument)
-{
-	my_data_t *md = new my_data_t;
-	md -> s = s;
-	md -> stop_flag = false;
-
-	pthread_create(&md -> th, NULL, thread, md);
-
-	return md;
-}
-
-extern "C" void uninit_plugin(void *arg)
-{
-	my_data_t *md = (my_data_t *)arg;
-
-	md -> stop_flag = true;
-
-	pthread_join(md -> th, NULL);
-
-	delete md;
-}
