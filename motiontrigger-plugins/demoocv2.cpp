cdb2cd48ccadafbe9e71e5f0381e5f45b7bfdab1
Folkert van Heusden <mail@vanheusden.com>
2017-10-15 22:15:02 +0200

    Detecting faces works better

--- motiontrigger-plugins/demoocv2.cpp
+++ motiontrigger-plugins/demoocv2.cpp
@@ -2,8 +2,10 @@
 #include <stdint.h>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
 #include <opencv2/objdetect/objdetect.hpp>
+// #include <opencv2/highgui/highgui.hpp>
 
 extern "C" {
 
@@ -16,30 +18,40 @@ void * init_motion_trigger(const char *const parameter)
 	// what you return here, will be given as a parameter
 	// to detect_motion
 
-	cv::HOGDescriptor *hog = new cv::HOGDescriptor();
+	cv::CascadeClassifier *face_cascade = new cv::CascadeClassifier();
+	if (!face_cascade -> load("/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"))
+		printf("load failed\n");
 
-	hog -> setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
-
-	return hog;
+	return face_cascade;
 }
 
 bool detect_motion(void *arg, const uint64_t ts, const int w, const int h, const uint8_t *const prev_frame, uint8_t *const current_frame, const uint8_t *const pixel_selection_bitmap)
 {
-	cv::HOGDescriptor *hog = (cv::HOGDescriptor *)arg;
+	cv::CascadeClassifier *face_cascade = (cv::CascadeClassifier *)arg;
 
-	cv::Mat curFrame = cv::Mat(w * h, 1, CV_8UC3, (void *)current_frame);
-	//curFrame = curFrame.reshape(0, h);
+	cv::Mat frame = cv::Mat(h, w, CV_8UC3, (void *)current_frame);
 
-	std::vector<cv::Rect> found;
-        hog -> detectMultiScale(curFrame, found, 0, cv::Size(8,8), cv::Size(32,32), 1.05, 2);
+	cv::Mat frame_gray;
+	cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+	cv::equalizeHist(frame_gray, frame_gray);
 
-	return !found.empty();
+//	cv::imwrite("gray.png", frame_gray);
+//	cv::imwrite("rgb.png", frame);
+//	exit(0);
+
+	std::vector<cv::Rect> faces;
+	face_cascade -> detectMultiScale(frame_gray, faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+
+	frame_gray.release();
+	frame.release();
+
+	return !faces.empty();
 }
 
 void uninit_motion_trigger(void *arg)
 {
 	// free memory etc
-	delete (cv::HOGDescriptor *)arg;
+	delete (cv::CascadeClassifier *)arg;
 }
 
 }
